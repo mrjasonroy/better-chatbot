98b06d3c707a7f1849e801aaa9fdc70a402ea43d
Jason Roy <jason@mrjasonroy.com>
2025-08-12 07:56:55 -0700

    Update app-sidebar-user.tsx

--- src/components/layouts/app-sidebar-user.tsx
+++ src/components/layouts/app-sidebar-user.tsx
@@ -36,7 +36,7 @@ import useSWR from "swr";
 import { getLocaleAction } from "@/i18n/get-locale";
 import { useCallback } from "react";
 import { GithubIcon } from "ui/github-icon";
-import { DiscordIcon } from "ui/discord-icon";
+// import { DiscordIcon } from "ui/discord-icon";
 import { useThemeStyle } from "@/hooks/use-theme-style";
 import { Session, User } from "better-auth";
 
